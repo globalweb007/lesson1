b5f82b2c19685f4c4f2f76992f686cb1624c477f
globalweb007 <globalwebmethods@gmail.com>
2022-11-29 17:17:46 -0800

    Adding index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+  <head>
+    <title>Lesson 1</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  </head>
+  <body>
+    <h1>Lession 1</h1>
+    <p>The html tags are the outer most tags and house the rest of the tags on an html page. The head tags are where we define some rules for the browser and load scripts like css and javascript. The title tag puts the title in the tab on your browser page. Metta tags tell the browser how to display the html content. The charset tells the browser to serve the content in UTF-8. More on that later. The viewport tag tells the browser that this document will be responsive for tablet and mobile. We will build on this document and add some scripts later.</p>
+    <p>In the body an h1 is the largest formating of text and is the Main Heading the google looks for when indexing you page for search results. It should alway be on a page, should only be one, and should be the subject of the page. A p tag is a paragraph tag and is used to display text.<p>
+  </body>
+</html>
